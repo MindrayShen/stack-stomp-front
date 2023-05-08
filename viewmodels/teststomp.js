b70bad7c72bfcda3ef9ab03b2d518f4689ca284a
slw <1159148321@qq.com>
2023-05-08 13:36:46 +0800

    init

--- viewmodels/teststomp.js
+++ viewmodels/teststomp.js
@@ -2,7 +2,12 @@ var app = new Vue({
     el: '#app',
     data: {
         socket: '',
-        stompclient: ''
+        stompclient: '',
+        input: '',
+        sendInput:'',
+        textarea1:'',
+        raceId:'',
+        userName:'',
     },
     mounted() {
         console.log('view mounted');
@@ -11,15 +16,21 @@ var app = new Vue({
         handleConnect() {
             console.log('connect click');
 
-            this.socket = new SockJS('http://localhost:8080/cjfstompsrv2');
-            this.stompclient = Stomp.over(this.socket);
+            // this.socket = new SockJS('http://localhost:8080/cjfstompsrv2');
+            // this.stompclient = Stomp.over(this.socket);
+            this.stompclient = Stomp.client(this.input);
             this.stompclient.connect({
                 // Authorization:"Bearer eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6ImY1N2ZmY2NiLWFlNmYtNDBjNi05OWQ2LWVkMjdiYjRiZDk1ZSJ9.ILT6dN-2AZu9wGX8Wjv5gqbLX-pDp18pL3PiHTnaZCn-o9VVxrD_szJhMIT0LPTt92hglBtp-pFFvNKTJ7cR2w",
-                name:"12345",
+                name:this.userName,
             }, frame => {
                 console.log(frame);
                 this.stompclient.subscribe('/user/mytopic/greetings', function (data) {
                     console.log(data)
+                    app.privateSend(data.body)
+                });
+                this.stompclient.subscribe('/myqueue/'+this.raceId, function (data) {
+                    console.log(data)
+                    app.publicSend(data.body)
                 });
             });
 
@@ -27,11 +38,32 @@ var app = new Vue({
         handleSend() {
             console.log('send click');
 
-            this.stompclient.send("/myapp/greeting", {}, 'hello websocket stomp');
+            this.stompclient.send("/myapp/greeting", {}, this.sendInput);
+        },
+        handleSendQueue() {
+            console.log('send click');
+
+            this.stompclient.send("/myapp/queue/"+this.raceId, {}, this.sendInput);
         },
         handleDisconnect(){
             console.log('disconnect click');
             this.stompclient.disconnect();
+        },
+        privateSend(message){
+            app.textarea1 += message + "\n"
+            this.$notify.success({
+                title: '这是一条私信',
+                message: message,
+                showClose: false
+            });
+        },
+        publicSend(message){
+            app.textarea1 += message + "\n"
+            this.$notify.success({
+                title: '这是一条比赛参与者消息',
+                message: message,
+                showClose: false
+            });
         }
     }
 })
